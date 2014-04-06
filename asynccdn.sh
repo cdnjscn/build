e8141bbd341c049b36c520400cdc37d096aaadd3
wxnet2013 <wxnet2013@gmail.com>
2014-04-06 22:40:25 +0800

    Delete asynccdn.sh

--- asynccdn.sh
+++
@@ -1,8 +0,0 @@
-cd /home/wxnet2013/build
-cd ../cdnjs
-git pull
-lftp -c "set ftp:list-options -a;
-open -u sys_upload/cdnjscn,Aj17bo-qkwe_1o? -p 21 v0.ftp.upyun.com; 
-lcd /home/wxnet2013/cdnjs/ajax/libs/;
-cd /libs;
-mirror --reverse --only-newer --only-missing --delete --use-cache --verbose --parallel=2 --exclude-glob .git --exclude-glob .DS_Store"
